168fbc8d9e9922f7889334c13547e4ada3b3cab9
Tushar kumar <103557760+Tusharkumar10@users.noreply.github.com>
2022-09-14 14:47:16 +0530

    Add files via upload

---
+++ AREA_OF_.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int r;
+float area;
+clrscr();
+printf("Enter the radius of circle\n");
+scanf("%d",&r);
+area=3.14*r*r;
+printf("Area of circle =  %f ",area);
+getch();
+
+}
+
