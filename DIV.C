c59c43d0a94282c0bd57fa0beb54fd872c84807e
Tushar kumar <103557760+Tusharkumar10@users.noreply.github.com>
2022-09-14 14:54:16 +0530

    Update DIV.C

--- DIV.C
+++ DIV.C
@@ -2,11 +2,11 @@
 #include<conio.h>
 void main()
 {
-int a,b,c;
+float a,b,c;
 clrscr();
-printf("Enter two numbers to add\n");
-scanf("%d %d",&a,&b);
+printf("Enter two numbers to divide\n");
+scanf("%f %f",&a,&b);
 c=a/b;
-printf("Division = %d",c);
+printf("Division = %f",c);
 getch();
 }
