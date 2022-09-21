f25edfd19c533a28089e755790b960a042612c0b
Tushar kumar <103557760+Tusharkumar10@users.noreply.github.com>
2022-09-21 19:31:28 +0530

    First 100 prime numbers

--- PRIME_HU.C
+++ PRIME_HU.C
@@ -11,7 +11,7 @@ printf("=====================1 to 100 prime numbers are=======================\n
 for(num=2;num<=100;num++)
 {
 flag=0;
-for(i=2;i<=sqrt(num);i++)
+for(i=1;i<=sqrt(num);i++)
 {
 if(num%i==0)
 {
